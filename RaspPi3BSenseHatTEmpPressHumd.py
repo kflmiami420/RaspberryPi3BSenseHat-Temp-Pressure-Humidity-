f0c5ed7469a9f60d5812fabbe7872d1d626d8f06
kflmiami420 <42599019+kflmiami420@users.noreply.github.com>
2018-09-20 18:21:38 -0400

    Create RaspPi3BSenseHatTEmpPressHumd.py

---
+++ RaspPi3BSenseHatTEmpPressHumd.py
@@ -0,0 +1,23 @@
+from sense_hat import SenseHat
+from datetime import datetime
+
+sense = SenseHat()
+
+def get_sense_data():
+        sense_data = []
+        sense_data.append(sense.get_temperature())
+        sense_data.append(sense.get_pressure())
+        sense_data.append(sense.get_humidity())
+
+        return sense_data
+
+def get_sense_data():
+  sense_data = []
+  sense_data.append(sense.get_temperature())
+  sense_data.append(sense.get_pressure())
+  sense_data.append(sense.get_humidity())
+
+  return sense_data
+
+while True:
+        print(get_sense_data())
